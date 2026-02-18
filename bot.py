2ee8b1bde0984e223c9e11618f573e9492783cf1
Алексей Мезенцев <alekseynick@inbox.ru>
2026-02-18 10:52:04 +0300

    Lucky message

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
 
+import random
 if __name__ == "__main__":
+    a = random.randint(0,1)
     print("Hello, let's chat!")
     print("I am the Queen!")
+    if a==0:
+        print("You're lucky")
+    else:
+        print("You aren't lucky")
+    
 
