32aa5dcefd89bc42840e520820fd6b2c2448959a
Алексей Мезенцев <alekseynick@inbox.ru>
2026-02-18 11:06:35 +0300

    Random numbers

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
-
+import random
 if __name__ == "__main__":
     print("Hello, let's chat!")
     print("I am the Queen!")
-
+    a = random.randint(0,1000)
+    b = random.randint(0,1000)
+    print(a)
+    print(b)
